91c54046216b3d4df3950f462517cb1492835d55
Mina Isaac Sabet Isaac <105015091+minaa66@users.noreply.github.com>
2024-02-04 13:30:28 +0200

    Update control.js
    add comments to make code readable

--- ESP/data/control.js
+++ ESP/data/control.js
@@ -1,7 +1,15 @@
+/*
+section 1 : intialize the websocket connection and handles login stuff (done)
+section 2 : handles the webpage ui (ongoing)
+section 3 : handles user messages (ongoing)
+*/
+
+
+
+
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
 document.getElementById('li1').classList.add('active');
-const username = localStorage.getItem('username');
 // Init web socket when the page loads
 window.addEventListener('load', onLoad);
 
@@ -11,7 +19,7 @@ function onLoad(event) {
 
    // Function to redirect to the login page
    function redirectToLoginPage() {
-    window.location.href = 'login.html'; // Change 'login.html' to your actual login page URL
+    window.location.href = 'login.html'; 
 }
 
 
@@ -38,9 +46,9 @@ function onOpen(event) {
     var login = {
         flag: "islogin?",
     }
-    websocket.send(JSON.stringify(login));
+    websocket.send(JSON.stringify(login));  // asks the server if the user have logged in or not 
     
-    sendStatus();
+    sendStatus();  //send lamp stutus upon open
 }
 
 function onClose(event) {
@@ -49,6 +57,9 @@ function onClose(event) {
     console.log('Connection closed');
     setTimeout(initWebSocket, 2000);
 }
+
+//function to ask the server to send the lamps stutus  
+//future stustus like supplies stutus can be placed her
 function sendStatus() {
     var vilampstutus = {
         vilampstutus: "send"
@@ -63,10 +74,11 @@ function sendStatus() {
 
 // Set interval to run every 5 seconds (5000 milliseconds)
 setInterval(sendStatus, 5000);
+//open a session for 10 minutes after this period user need to login again
 setInterval(redirectToLoginPage,600000);
 
 
-
+//ui function
 function showTab(tabId,li_id) {
     // Hide all tabs
     document.getElementById('li1').classList.add('active');
@@ -84,7 +96,125 @@ function showTab(tabId,li_id) {
 }
 
 
-function handleMessage(event) {    
+document.getElementById('uvin').addEventListener('change',toggleuv);
+document.getElementById('viin').addEventListener('change',togglevi);
+//ui function
+function toggleuv(){
+if(document.getElementById("uvstate").innerHTML==='on'){
+    var turnuvoff = {
+        turnuvoff: "turnoff"
+    }
+    // Assuming 'websocket' is your WebSocket object
+    websocket.send(JSON.stringify(turnuvoff));
+
+} 
+else {
+
+    var turnuvon = {
+        turnuvon: "turnon"
+    }
+    // Assuming 'websocket' is your WebSocket object
+    websocket.send(JSON.stringify(turnuvon));
+
+}
+}
+
+
+//ui function
+function togglevi(){
+if(document.getElementById("vistate").innerHTML==='on'){
+    var turnvioff = {
+        turnvioff: "turnoff"
+    }
+    // Assuming 'websocket' is your WebSocket object
+    websocket.send(JSON.stringify(turnvioff));
+
+} 
+else {
+    var turnvion = {
+        turnvion: "turnon"
+    }
+    // Assuming 'websocket' is your WebSocket object
+    websocket.send(JSON.stringify(turnvion));
+}
+}
+
+var curruntstate="none";
+
+//ui function
+hidelabel(); //intially shrink the sidebar 
+document.getElementById('li0').addEventListener('click',togglemenu)
+function showNav() {
+    const navElement = document.querySelector('nav');
+    navElement.style.width="30%";
+    showlabel();
+}
+//ui function
+function hideNav() {
+    const navElement = document.querySelector('nav');
+    
+    if(curruntstate==="none"){
+        navElement.style.width="6%";
+        hidelabel();
+    }
+    else{
+        navElement.style.width="30%";
+        showlabel();
+        }
+    }
+//ui function
+function togglemenu() {
+    const navElement = document.querySelector('nav');
+    if (curruntstate==="block") {
+        
+        navElement.style.width="6%";
+        hidelabel();
+
+        
+    } else {
+        navElement.style.width="30%";
+        showlabel();
+        curruntstate="block";
+    }
+}
+//ui function
+function hidelabel() {
+    curruntstate="none";
+    document.getElementById("li0").innerHTML="=";
+    document.getElementById("li0").style.fontSize="40px";
+    for(let i = 1; i <= 6; i++){
+    document.getElementById("li"+i).innerHTML=i;
+}
+}
+//ui function
+function showlabel(){
+    
+    for(let J = 1; J <= 6; J++){
+        switch(J){
+            
+            case 1:
+                document.getElementById("li"+J).innerHTML="Lamps test";
+                break;
+            case 2:
+                document.getElementById("li"+J).innerHTML="Supplies";
+                break;
+            case 3:
+                document.getElementById("li"+J).innerHTML="Motors";
+                break;
+            case 4:
+                document.getElementById("li"+J).innerHTML="Detector";
+                break;
+            case 5:
+                document.getElementById("li"+J).innerHTML="Date and Time";
+                break;
+            case 6:
+                document.getElementById("li"+J).innerHTML="Test tab";
+                break;
+        }
+        
+    }
+
+    function handleMessage(event) {    
     var myObj = JSON.parse(event.data);
     console.log(myObj);
     if(myObj.hasOwnProperty('flag')){
@@ -160,117 +290,5 @@ function handleMessage(event) {
     } 
     }
 
-document.getElementById('uvin').addEventListener('change',toggleuv);
-document.getElementById('viin').addEventListener('change',togglevi);
 
-function toggleuv(){
-if(document.getElementById("uvstate").innerHTML==='on'){
-    var turnuvoff = {
-        turnuvoff: "turnoff"
-    }
-    // Assuming 'websocket' is your WebSocket object
-    websocket.send(JSON.stringify(turnuvoff));
-
-} 
-else {
-
-    var turnuvon = {
-        turnuvon: "turnon"
-    }
-    // Assuming 'websocket' is your WebSocket object
-    websocket.send(JSON.stringify(turnuvon));
-
-}
-}
-
-
-
-function togglevi(){
-if(document.getElementById("vistate").innerHTML==='on'){
-    var turnvioff = {
-        turnvioff: "turnoff"
-    }
-    // Assuming 'websocket' is your WebSocket object
-    websocket.send(JSON.stringify(turnvioff));
-
-} 
-else {
-    var turnvion = {
-        turnvion: "turnon"
-    }
-    // Assuming 'websocket' is your WebSocket object
-    websocket.send(JSON.stringify(turnvion));
-}
-}
-
-var curruntstate="none";
-hidelabel();
-document.getElementById('li0').addEventListener('click',togglemenu)
-function showNav() {
-    const navElement = document.querySelector('nav');
-    navElement.style.width="30%";
-    showlabel();
-}
-function hideNav() {
-    const navElement = document.querySelector('nav');
-    
-    if(curruntstate==="none"){
-        navElement.style.width="6%";
-        hidelabel();
-    }
-    else{
-        navElement.style.width="30%";
-        showlabel();
-        }
-    }
-
-function togglemenu() {
-    const navElement = document.querySelector('nav');
-    if (curruntstate==="block") {
-        
-        navElement.style.width="6%";
-        hidelabel();
-
-        
-    } else {
-        navElement.style.width="30%";
-        showlabel();
-        curruntstate="block";
-    }
-}
-function hidelabel() {
-    curruntstate="none";
-    document.getElementById("li0").innerHTML="=";
-    document.getElementById("li0").style.fontSize="40px";
-    for(let i = 1; i <= 6; i++){
-    document.getElementById("li"+i).innerHTML=i;
-}
-}
-
-function showlabel(){
-    
-    for(let J = 1; J <= 6; J++){
-        switch(J){
-            
-            case 1:
-                document.getElementById("li"+J).innerHTML="Lamps test";
-                break;
-            case 2:
-                document.getElementById("li"+J).innerHTML="Supplies";
-                break;
-            case 3:
-                document.getElementById("li"+J).innerHTML="Motors";
-                break;
-            case 4:
-                document.getElementById("li"+J).innerHTML="Detector";
-                break;
-            case 5:
-                document.getElementById("li"+J).innerHTML="Date and Time";
-                break;
-            case 6:
-                document.getElementById("li"+J).innerHTML="Test tab";
-                break;
-        }
-        
-    }
 }
