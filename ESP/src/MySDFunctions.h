710955c47a4c382518c4a194759c0af56e0046c5
mina <imina3951@gmail.com>
2024-06-21 18:48:25 -0700

    oh yeah

--- ESP/src/MySDFunctions.h
+++ ESP/src/MySDFunctions.h
@@ -3,17 +3,9 @@
 
 #include <ArduinoJson.h>
 #include <SD.h>
-
-bool writeToDatabase(const char *basePath, const DynamicJsonDocument &doc);
-DynamicJsonDocument readFromDatabase(const char* filename);
 bool SdWriteString(String &filename,String &content);
-String createJsonString(String key, String value);
-String createJsonString(String key, int value) ;
-DynamicJsonDocument getFilesJson(const char *directory);
-void ReadFromDBTask(const DynamicJsonDocument &doc);
-String WriteAsCsv(const DynamicJsonDocument& doc, int numKeys);
-String csvToJsonString(const String& csvLine);
 bool deleteLineFromFile(String filename, String searchName);
+
 
 //void handleread(const DynamicJsonDocument &doc)
 //bool deleteLineFromFile(const char* filename, const char* searchName);
